997505a40047de7ea92b8a24064a35e0dac100ac
Yuichi Motohashi <bookbridge.y@gmail.com>
2016-03-01 14:22:26 +0100

    Fix css styles

--- design_process/settings.py
+++ design_process/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'process_flow',
     'bootstrapform',
     'debug_toolbar',
+    'markdown_deux',
     )
 
 
@@ -94,7 +95,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
-
-
-
