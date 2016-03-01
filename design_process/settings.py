9991f77b906eb490c1a6cc97a54d65b1aae93ea0
Yuichi Motohashi <bookbridge.y@gmail.com>
2016-03-02 00:25:50 +0900

    fix setting.py & requirement.txt

--- design_process/settings.py
+++ design_process/settings.py
@@ -93,6 +93,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = os.path.joins(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
